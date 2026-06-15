f0f687a9f4ddc2b4b472b200464a5d9141671956
Moe Charm (CI) <moecharm@example.com>
2026-06-16 08:17:54 +0900

    Refactor HZ6 workload capacity builder

--- hakozuna-hz6/linux/build_hz6_preload_workload_capacity_target.sh
+++ hakozuna-hz6/linux/build_hz6_preload_workload_capacity_target.sh
@@ -10,43 +10,36 @@ HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS=()
 hz6_preload_profile_selected_cflags HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS 1
 HZ6_WORKLOAD_CAPACITY_OUT_DIR="${ROOT_DIR}/hakozuna-hz6/out/linux/hz6_preload_workload_capacity_target"
 
+hz6_workload_capacity_set_tables() {
+  local route_capacity="$1"
+  local descriptor_capacity="$2"
+  local source_block_capacity="$3"
+
+  hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
+    HZ6_ROUTE_TABLE_CAPACITY "$route_capacity"
+  hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
+    HZ6_OBJECT_DESCRIPTOR_CAPACITY "$descriptor_capacity"
+  hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
+    HZ6_SOURCE_BLOCK_CAPACITY "$source_block_capacity"
+}
+
 case "${HZ6_WORKLOAD_CAPACITY_LEVEL:-full}" in
   lite)
     HZ6_WORKLOAD_CAPACITY_OUT_DIR="${ROOT_DIR}/hakozuna-hz6/out/linux/hz6_preload_workload_capacity_lite_target"
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_ROUTE_TABLE_CAPACITY 65536
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_OBJECT_DESCRIPTOR_CAPACITY 16384
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_SOURCE_BLOCK_CAPACITY 2048
+    hz6_workload_capacity_set_tables 65536 16384 2048
     ;;
   lite_map8192)
     HZ6_WORKLOAD_CAPACITY_OUT_DIR="${ROOT_DIR}/hakozuna-hz6/out/linux/hz6_preload_workload_capacity_lite_map8192_target"
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_ROUTE_TABLE_CAPACITY 65536
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_OBJECT_DESCRIPTOR_CAPACITY 16384
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_SOURCE_BLOCK_CAPACITY 2048
+    hz6_workload_capacity_set_tables 65536 16384 2048
     hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
       HZ6_TOY_SMALL_ACTIVE_FREE_MAP_CAPACITY 8192
     ;;
   mid)
     HZ6_WORKLOAD_CAPACITY_OUT_DIR="${ROOT_DIR}/hakozuna-hz6/out/linux/hz6_preload_workload_capacity_mid_target"
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_ROUTE_TABLE_CAPACITY 98304
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_OBJECT_DESCRIPTOR_CAPACITY 24576
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_SOURCE_BLOCK_CAPACITY 3072
+    hz6_workload_capacity_set_tables 98304 24576 3072
     ;;
   full)
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_ROUTE_TABLE_CAPACITY 131072
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_OBJECT_DESCRIPTOR_CAPACITY 32768
-    hz6_preload_replace_define HZ6_WORKLOAD_CAPACITY_TARGET_CFLAGS \
-      HZ6_SOURCE_BLOCK_CAPACITY 4096
+    hz6_workload_capacity_set_tables 131072 32768 4096
     ;;
   *)
     echo "unknown HZ6_WORKLOAD_CAPACITY_LEVEL: ${HZ6_WORKLOAD_CAPACITY_LEVEL}" >&2
